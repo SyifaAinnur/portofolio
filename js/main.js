436910f2562dbdac37ad1fe47c7afe0a4ead109b
Syifa Ainnur <arwahitem01@gmail.com>
2021-10-10 19:38:25 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,80 +1,96 @@
-
-
-
 var domain = "https://syifaainnur.github.io/portofolio/";
-$(document).ready(function() {
-    $.ajax({
-        type: 'GET',
-        url: domain + "home.html",
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function(result) {
-            $("#content").html(result);
-        }
-    });
+$(document).ready(function () {
+  $.ajax({
+    type: "GET",
+    url: domain + "home.html",
+    cache: false,
+    processData: false,
+    contentType: false,
+    success: function (result) {
+      $("#content").html(result);
+    },
+  });
 });
 
-$('a.home').bind('click', function(event) {
-    $.ajax({
-        type: 'GET',
-        url: domain + "home.html",
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function(result) {
-            $("#content").html(result);
-        }
-    });
+$("a.home").bind("click", function (event) {
+  $.ajax({
+    type: "GET",
+    url: domain + "home.html",
+    cache: false,
+    processData: false,
+    contentType: false,
+    success: function (result) {
+      $("#content").html(result);
+    },
+  });
 });
-$('a.about').bind('click', function(event) {
-    $.ajax({
-        type: 'GET',
-        url: domain + "about.html",
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function(result) {
-            $("#content").html(result);
-        }
-    });
-});
-
-$('a.skill').bind('click', function(event) {
-    $.ajax({
-        type: 'GET',
-        url: domain + "skill.html",
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function(result) {
-            $("#content").html(result);
-        }
-    });
+$("a.about").bind("click", function (event) {
+  $.ajax({
+    type: "GET",
+    url: domain + "about.html",
+    cache: false,
+    processData: false,
+    contentType: false,
+    success: function (result) {
+      $("#content").html(result);
+    },
+  });
 });
 
-$('a.contact').bind('click', function(event) {
-    $.ajax({
-        type: 'GET',
-        url: domain + "portofolio.html",
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function(result) {
-            $("#content").html(result);
-        }
-    });
-});
-$('a.contact').bind('click', function(event) {
-    $.ajax({
-        type: 'GET',
-        url: domain + "qoute.html",
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function(result) {
-            $("#content").html(result);
-        }
-    });
+$("a.skill").bind("click", function (event) {
+  $.ajax({
+    type: "GET",
+    url: domain + "skill.html",
+    cache: false,
+    processData: false,
+    contentType: false,
+    success: function (result) {
+      $("#content").html(result);
+    },
+  });
 });
 
+$("a.contact").bind("click", function (event) {
+  $.ajax({
+    type: "GET",
+    url: domain + "portofolio.html",
+    cache: false,
+    processData: false,
+    contentType: false,
+    success: function (result) {
+      $("#content").html(result);
+    },
+  });
+});
+$("a.contact").bind("click", function (event) {
+  $.ajax({
+    type: "GET",
+    url: domain + "qoute.html",
+    cache: false,
+    processData: false,
+    contentType: false,
+    success: function (result) {
+      $("#content").html(result);
+    },
+  });
+});
+const text = document.getElementById("quote");
+const author = document.getElementById("author");
+const tweetButton = document.getElementById("tweet");
+
+const getNewQuote = async () => {
+  var url = "https://type.fit/api/quotes";
+
+  const response = await fetch(url);
+  console.log(typeof response);
+  const allQuotes = await response.json();
+  const indx = Math.floor(Math.random() * allQuotes.length);
+  const quote = allQuotes[indx].text;
+  const auth = allQuotes[indx].author;
+
+  if (auth == null) {
+    author = "Anonymous";
+  }
+  //tweet the quote
+};
+getNewQuote();
